d109f6b65b07295f57e28c96fb09fb08959046b8
ecin <ecin@copypastel.com>
2010-08-27 22:34:29 -0700

    Pushed to showing an index page with layout. Time for the real fun to begin.

---
+++ server.js
@@ -0,0 +1,18 @@
+// BSD licensed, rock along with it.
+
+var sys = require('sys');
+var connect = require('connect');
+var express = require('express');
+var app = express.createServer();
+
+// Config
+
+app.configure( function(){
+  app.use(express.staticProvider(__dirname + '/public'));
+    });
+
+app.get('/', function(req, res){
+    res.render('index.ejs');
+    });
+
+app.listen(3000);
