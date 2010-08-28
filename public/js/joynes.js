45b0dc2b884e0d126f9100a7c03158f3de67c647
☁ daicoden <daicoden@copypastel.com>
2010-08-28 02:47:49 -0700

    Files now in the right place.

---
+++ public/js/joynes.js
@@ -0,0 +1,5 @@
+$.getScript('')
+<script type="text/javascript" src="jquery-1.4.2.js"></script>
+<script type="text/javascript" src="dynamicaudio-min.js"></script>
+<script type="text/javascript" src="jquery.dimensions.min.js"></script>
+<script type="text/javascript" src="jsnes.js"></script>
--- public/js/tools.js
+++ public/js/tools.js
@@ -1,7 +1,9 @@
+include "inspector"
+
 inspector = new Inspector();
 
 inspector.logger = new Logger('console');
-inspector.attach(nes.mmap, nes.mmap.regWrite);
+inspector.attach(nes1.mmap, nes1.mmap.regWrite);
 
 inspector.filter = functio(args) {
   // Filter options in here
