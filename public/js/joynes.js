ff0c47cbae68a3a33f669421a7d85f9b51a57bce
ecin <ecin@copypastel.com>
2010-08-28 10:48:05 -0700

    Got client sending a message over socket whenever it loads a ROM. Now to use that to get other clients to load it as well.

--- public/js/joynes.js
+++ public/js/joynes.js
@@ -10,3 +10,4 @@ $.include = function(url){
 $.include('/js/dynamicaudio-min.js');
 $.include('/js/jquery.dimensions.min.js');
 $.include('/js/jsnes.js');
+$.include('/js/messages.js');
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+// Quick fix for npm installs under Mac OS X
+require.paths.push('/usr/local/lib/node')
+
 // BSD licensed, rock along with it.
 
 var sys = require('sys');
 //var connect = require('connect');
 var express = require('express');
+var io = require('socket.io');
 var app = express.createServer();
 
 // Config
@@ -14,5 +18,28 @@ app.configure( function(){
 app.get('/', function(req, res){
     res.render('index.ejs');
     });
+
+var socket = io.listen(app);
+
+socket.on('connection', function(client){
+	// client.send({ buffer: buffer });
+	// We need to create a channel between 
+	// Since we're only having two clients connecting right now, broadcasting should be fine.
+	// Prolly gonna want to send a message on loadRom to do the same on another client
+	// client.broadcast({ announcement: client.sessionId + ' connected' }, [client_ids]);
+
+	client.on('message', function(message){
+		var msg = { message: [client.sessionId, message] };
+		//buffer.push(msg);
+		//if (buffer.length > 15) buffer.shift();
+		client.broadcast(msg);
+    sys.puts(message);
+	});
+
+	client.on('disconnect', function(){
+		//client.broadcast({ announcement: client.sessionId + ' disconnected' });
+		sys.puts( "Disconnected: " + client.sessionId );
+	});
+});
 
 app.listen(80);
